a1dead58e0e3b519b5ba7285de58760dc21e0dbf
Thomas Weise <tweise@hfuu.edu.cn>
2020-08-29 11:52:52 +0800

    more equations

--- 07_simulated_annealing/code/TemperatureSchedule_Exponential.java
+++ 07_simulated_annealing/code/TemperatureSchedule_Exponential.java
@@ -2,10 +2,10 @@ package aitoa.algorithms;
 
 public class Exponential extends TemperatureSchedule {
 // unnecessary things omitted here
-  public final double epsilon;
+  public final double epsilon; // (*@\listingCommentFormat{$\equiv\;\varepsilon$}@*)
 
   public double temperature(long tau) {
-    // (*@\listingCommentFormat{$T(\iteration) = T_s * (1 - \epsilon) ^ {\iteration - 1}$}@*)
+    // (*@\listingCommentFormat{$T(\iteration) = T_s * (1 - \varepsilon) ^ {\iteration - 1}$}@*)
     return (this.startTemperature * Math.pow((1d - this.epsilon), (tau - 1L)));
   }  
 }
